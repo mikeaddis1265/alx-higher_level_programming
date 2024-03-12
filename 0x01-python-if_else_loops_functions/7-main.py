be145f735e655deebd99b1ec34b614195756b414
mikeaddis1265 <michaeladdis78@gmail.com>
2024-03-12 19:40:40 +0300

    done islower

---
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+islower = __import__('7-islower').islower
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("A is {}".format("lowre" if islower("A") else "upper"))
