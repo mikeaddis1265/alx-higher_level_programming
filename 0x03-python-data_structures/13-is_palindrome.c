5d70d73f2fd237b2bce5df0a843bec6ce2ae0b27
mikeaddis1265 <michaeladdis78@gmail.com>
2024-03-23 22:06:19 +0300

    done

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,41 +2,54 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * is_palindrome- checks if a singly linked list is a palindrome
+ * is_palindrome - checks if a singly linked list is a palindrome
  * 
- * @head: pointer to head- pointer
+ * @head: pointer to head-pointer
  * 
  * return 0 if not, or 1 if it is
-*/
+ */
 int is_palindrome(listint_t **head)
 {
-    listint_t *temp;
-    listint_t *traversal;
-    listint_t *pre;
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *middle = NULL;
+    listint_t *second_half = NULL;
+    listint_t *current = NULL;
 
-    traversal = *head;
-    temp = *head;
-    pre = *head;
+    if (head == NULL || *head == NULL)
+        return 1;
 
-    int palindrome_true = 1;
+    while (fast != NULL && fast->next != NULL)
 
-    while (temp != NULL)
-    {
-        while(traversal->next != NULL)
-
-        {
-            pre = traversal;
-            traversal = traversal->next;
-            
-        }
-    
-
-    if (traversal->n != temp->n)
-        {
-            palindrome_true = 0;
-            return palindrome_true;
-        }
-        temp = temp->next
+        fast = fast->next->next;
+        prev = slow;
+        slow = slow->next;
     }
-        return 0;
+    {
+    middle = slow;
+    second_half = middle->next;
+    middle->next = NULL; // Split the list into two halves
+
+    // Reverse the second half of the list
+    current = second_half;
+    while (current != NULL)
+    {
+        listint_t *next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    second_half = prev; // Now, second_half points to the head of the reversed second half
+
+    while (second_half != NULL)
+    {
+        if (second_half->n != (*head)->n)
+            return 0; // If any pair of nodes is not equal, return 0
+        second_half = second_half->next;
+        *head = (*head)->next; // Move to the next node in the first half
+    }
+
+    return 1; // If all pairs of nodes are equal, return 1
 }
