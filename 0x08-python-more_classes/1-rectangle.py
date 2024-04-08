751e7ecbb6ec90efe8e7669637bf6e79e324f074
mikeaddis1265 <michaeladdis78@gmail.com>
2024-04-08 14:51:19 +0300

    done

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,52 @@
+#!usr/bin/python3
+"""Real definition of a reactangle"""
+
+
+class Rectangle:
+
+    """initialization of an instance object"""
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def height(self):
+        """return the size
+        Returns:
+            height
+        """
+        return (self.__height)
+
+    """setter and getter functions"""
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """return the size
+        Returns:
+            width
+        """
+        return (self.__height)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
