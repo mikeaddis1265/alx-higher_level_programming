e447f4d50757d227b1fa27f58f7334d51d9f339e
mikeaddis1265 <michaeladdis78@gmail.com>
2024-06-18 22:40:52 +0300

    fix error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,21 +3,24 @@ const { argv } = require('node:process');
 if (argv.length <= 3 ){
     console.log(0);
 }
+else{
 
-let first = Number(argv[2]);
-let second = Number(argv[2]);
+    let first = Number(argv[2]);
+    let second = Number(argv[2]);
 
-for (let i = 3; i <= argv.length; i++){
-    if ( Number(argv[i]) > first){
-        let temp = first;
-        first = Number(argv[i]);
-        second = temp;
-    }
-    else{
-        if (Number(argv[i] > second) || (first == second)){
-            second = Number(argv[i]);
+    for (let i = 3; i <= argv.length; i++){
+        if ( Number(argv[i]) > first){
+            let temp = first;
+            first = Number(argv[i]);
+            second = temp;
         }
-       
+        else{
+            if (Number(argv[i] > second) || (first == second)){
+                second = Number(argv[i]);
+            }
+        
+        }
     }
+    console.log(second)
+
 }
-console.log(second)
