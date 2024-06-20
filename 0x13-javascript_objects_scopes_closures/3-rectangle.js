638efb36601ef5fa7634650e307ade6ef753e3f6
mikeaddis1265 <michaeladdis78@gmail.com>
2024-06-20 11:54:37 +0300

    print rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+class Rectangle{
+    constructor(w, h){
+        if (h >0 && w > 0){
+            this.width = w;
+            this.height = h;
+        }
+        
+    }
+    print(){
+        for (let i = 0; i < this.height; i++){
+            let str = ""
+            for (let j = 0; j < this.width; j++){
+                str += 'X';
+            }
+            console.log(str);
+        }
+    }
+}
+module.exports = Rectangle;
