3cbea49150c442aaca898ec18631102a3bae3450
mikeaddis1265 <michaeladdis78@gmail.com>
2024-06-23 18:09:24 +0300

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", usr=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cu = db.cursor()  # Corrected variable name
-    cu.execute("SELECT * FROM states ")  # Corrected variable name
+    cu.execute("SELECT * FROM states ORDER BY id ASC")  # Corrected variable name
     rows = cu.fetchall()  # Corrected variable name
     for row in rows:
         print(row)
