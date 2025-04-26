5fd5ddb9d9cfd52b74797ee5662fecbbd0ae8dea
unknown <michaeladdis78@gmail.com>
2025-04-26 22:58:47 +0300

    Update failing test to trigger bug creation

--- 0x00-python-hello_world/test_force_fail.py
+++ 0x00-python-hello_world/test_force_fail.py
@@ -1,2 +1,2 @@
 def test_force_fail():
-    assert False, "This test is designed to always fail and trigger bug creation." 
+    assert False, "This test is designed to always fail and trigger bug creation - updated" 
